777ab272d7a39e8ed932c4ae2a1ffee603a12ab2
Allan <allan.mendes0976@gmail.com>
2024-09-30 21:37:05 -0300

    spyOn

--- src/services/persistency.spec.ts
+++ src/services/persistency.spec.ts
@@ -1,8 +1,32 @@
-/*
+import { Persistency } from './persistency';
 
+describe('Persistency', () => {
+  afterEach(() => jest.clearAllMocks()); //Depois de cada teste limpa os mocks
+
+  it('should return undefined', () => {
+    //System under test
+    const sut = new Persistency();
+    expect(sut.saveOrder()).toBeUndefined();
+  });
+
+  it('should call console.log once', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call console.log with "Pedido salvo com sucesso..."', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledWith('Pedido salvo com sucesso...');
+  });
+});
+/*
 Describe(): Para testar em grupo, posso ter mais de um describe e também é possível colocar um dentro do outro.
 
-*/
+
 describe('Descrição: Grupos de teste 1', () => {
   it('Descrição do teste (it)', () => {
     const number = 1;
@@ -25,3 +49,4 @@ test('Descrição do teste (test) 4', () => {
   const nome = 'Allan';
   expect(nome).toBe('Allan');
 });
+*/
